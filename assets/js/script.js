66a8f9d2edf62d2b18e8387b38aad19ae9a3e105
Maja Kazmierczak <upgbhvet@students.codeinstitute.net>
2024-11-25 14:31:46 +0000

    Display addition question for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,42 @@
 // Wait for DOM to load before running the game
 // Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", function () {
-  letbuttons = document.getElementsByTagName("button");
+
+document.addEventListener("DOMContentLoaded", function() {
+  let buttons = document.getElementsByTagName("button");
 
   for (let button of buttons) {
-    button.addEventListener("click", function () {
-      if (this.getAttribute("data-type") === "submit") {
-        alert("You clicked Submit!");
-      } else {
-        let gameType = this.getAttribute("data-type");
-        alert(`You clicked ${gameType}`);
-      }
-    });
+      button.addEventListener("click", function() {
+          if (this.getAttribute("data-type") === "submit") {
+              alert("You clicked Submit!");
+          } else {
+              let gameType = this.getAttribute("data-type");
+              runGame(gameType);
+          }
+      });
   }
+
+  runGame("addition");
 });
 
-// a docstring shows a pop up when the function is hovered over in a different place in the doc - it serves as a quick reminder of the funciton role. 
+// a docstring shows a pop up when the function is hovered over in a different place in the doc - it serves as a quick reminder of the funciton role.
 
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame() {
-    // creates two random numbers between 1 and 25
-    let num1 = Math.floor(Math.random() * 25) + 1;
-    let num2 = Math.floor(Math.random() * 25) + 1;
+function runGame(gameType) {
+  // creates two random numbers between 1 and 25
+  let num1 = Math.floor(Math.random() * 25) + 1;
+  let num2 = Math.floor(Math.random() * 25) + 1;
+
+  if ((gameType === "addition")) {
+    displayAdditionQuestion(num1, num2);
+  } else {
+    alert(`Unknown game type: ${gameType}`);
+    // throw statement will stop the game from running & show an error message in the console
+    throw `Unknown game type: ${gameType}. Aborting!`;
+  }
 }
 
 function checkAnswer() {}
@@ -36,7 +47,11 @@ function incrementScore() {}
 
 function incrementWrongAnswer() {}
 
-function displayAdditionQuestion() {}
+function displayAdditionQuestion(operand1, operand2) {
+  document.getElementById("operand1").textContent = operand1;
+  document.getElementById("operand2").textContent = operand2;
+  document.getElementById("operator").textContent = "+";
+}
 
 function displaySubtractQuestion() {}
 
