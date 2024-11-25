942270b68f58bf8715711b0ed4ea8f0bb3775d6f
Maja Kazmierczak <upgbhvet@students.codeinstitute.net>
2024-11-25 15:00:30 +0000

    Add code to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,18 @@
 // Wait for DOM to load before running the game
 // Get the button elements and add event listeners to them
 
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   let buttons = document.getElementsByTagName("button");
 
   for (let button of buttons) {
-      button.addEventListener("click", function() {
-          if (this.getAttribute("data-type") === "submit") {
-              checkAnswer(); 
-          } else {
-              let gameType = this.getAttribute("data-type");
-              runGame(gameType);
-          }
-      });
+    button.addEventListener("click", function () {
+      if (this.getAttribute("data-type") === "submit") {
+        checkAnswer();
+      } else {
+        let gameType = this.getAttribute("data-type");
+        runGame(gameType);
+      }
+    });
   }
 
   runGame("addition");
@@ -31,7 +30,7 @@ function runGame(gameType) {
   let num2 = Math.floor(Math.random() * 25) + 1;
 
   // two or three equal signs are a comparison operator (comparing two+ values), one equal sign is an assignment operator (assigns a value)
-  if ((gameType === "addition")) {
+  if (gameType === "addition") {
     displayAdditionQuestion(num1, num2);
   } else {
     alert(`Unknown game type: ${gameType}`);
@@ -50,12 +49,13 @@ function checkAnswer() {
   let isCorrect = userAnswer === calculatedAnswer[0];
   if (isCorrect) {
     alert("Hey! You got it right! :D");
+    incrementScore();
   } else {
-    alert(`Awwww...you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
-  }
+    alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    incrementWrongAnswer();
+}
 
   runGame(calculatedAnswer[1]);
-
 }
 
 /**
@@ -76,12 +76,30 @@ function calculateCorrectAnswer() {
     // throw statement will stop the game from running & show an error message in the console
     throw `Unimplemented operator ${operator}. Aborting!`;
   }
+}
+
+/**
+ * gets the current score from the DOM and increments it by 1
+ */
+function incrementScore() {
+
+  let oldScore = parseInt(document.getElementById("score").innerText);
+  // generally speaking, innertext & textContent are interchangeable with some small differences
+  // compound addition operator (+=) adds the value of the right operand to a variable and assigns the result to the variable
+  document.getElementById("score").innerText = ++oldScore;
 
 }
 
-function incrementScore() {}
+/**
+ * gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
+function incrementWrongAnswer() {
 
-function incrementWrongAnswer() {}
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    // generally speaking, innertext & textContent are interchangeable with some small differences
+    // compound addition operator (+=) adds the value of the right operand to a variable and assigns the result to the variable
+    document.getElementById("incorrect").innerText = ++oldScore;
+}
 
 function displayAdditionQuestion(operand1, operand2) {
   document.getElementById("operand1").textContent = operand1;
